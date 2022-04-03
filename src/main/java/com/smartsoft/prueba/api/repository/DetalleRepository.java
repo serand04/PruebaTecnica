d3f81bec91ebc22e78c5461add0f473fcb51136c
Andrés <50032680+serand04@users.noreply.github.com>
2022-04-03 12:42:02 -0500

    Update DetalleRepository.java

--- src/main/java/com/smartsoft/prueba/api/repository/DetalleRepository.java
+++ src/main/java/com/smartsoft/prueba/api/repository/DetalleRepository.java
@@ -3,6 +3,7 @@ package com.smartsoft.prueba.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.smartsoft.prueba.api.model.Detalle;
 
+//Interface que trae la clase Detalle y un variable Long
 public interface DetalleRepository extends JpaRepository<Detalle, Long>{
 
 }
