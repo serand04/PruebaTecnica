abd021632c03c95cdbf325e20ae3c40c28274cae
Andrés <50032680+serand04@users.noreply.github.com>
2022-04-03 13:09:20 -0500

    Update PruebaSmartSoftApplication.java

--- src/main/java/com/smartsoft/prueba/api/PruebaSmartSoftApplication.java
+++ src/main/java/com/smartsoft/prueba/api/PruebaSmartSoftApplication.java
@@ -3,6 +3,10 @@ package com.smartsoft.prueba.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Clase para inciar SpringBoot
+ */
+
 @SpringBootApplication
 public class PruebaSmartSoftApplication {
 
