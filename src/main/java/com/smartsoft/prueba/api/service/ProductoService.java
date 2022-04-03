68b57fcff075e3d1b3f1d2a2c091f585e6b56335
Andrés <50032680+serand04@users.noreply.github.com>
2022-04-03 12:53:20 -0500

    Update ProductoService.java

--- src/main/java/com/smartsoft/prueba/api/service/ProductoService.java
+++ src/main/java/com/smartsoft/prueba/api/service/ProductoService.java
@@ -9,24 +9,34 @@ import org.springframework.stereotype.Service;
 import com.smartsoft.prueba.api.model.Producto;
 import com.smartsoft.prueba.api.repository.ProductoRepository;
 
+/*
+ * Clase de servicio del Producto
+ * En esta clase es donde se crea, lista, borra y busca el producto
+ */
+
 @Service
 public class ProductoService {
 	
+	//Atributo que trae la interface de Producto
 	@Autowired
 	private ProductoRepository productoRepository;
 	
+	//Método para crear un producto
 	public Producto create(Producto producto) {
 		return productoRepository.save(producto);
 	}
 	
+	//Método para listar todos los productos
 	public List<Producto> getAllProductos() {
 		return productoRepository.findAll();
 	}
 	
+	//Método para borrar un producto
 	public void delete(Producto producto) {
 		productoRepository.delete(producto);
 	}
 	
+	//Método para buscar los productos por ID
 	public Optional<Producto> findById(Long id) {
 		return productoRepository.findById(id);
 	}
