d453d0e742b8faa5325e39df8e9a1e0fc3bcb49d
Andrés <50032680+serand04@users.noreply.github.com>
2022-04-03 12:43:24 -0500

    Update ProductoRepository.java

--- src/main/java/com/smartsoft/prueba/api/repository/ProductoRepository.java
+++ src/main/java/com/smartsoft/prueba/api/repository/ProductoRepository.java
@@ -3,6 +3,7 @@ package com.smartsoft.prueba.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.smartsoft.prueba.api.model.Producto;
 
+//Interface que trae la clase Producto y un variable Long
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
 
 }
